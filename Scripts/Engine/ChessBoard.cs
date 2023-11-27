0c24667b0bfb060d60bd2372b90e02c96d940f3d
SeanInSpace <50900457+SeanInSpace@users.noreply.github.com>
2023-11-27 12:15:23 -0600

    HorizontalMove.cs changes

--- Scripts/Engine/ChessBoard.cs
+++ Scripts/Engine/ChessBoard.cs
@@ -1,29 +1,25 @@
 using System;
 using Godot;
 
-public partial class ChessBoard : Node2D
-{
+public partial class ChessBoard : Node2D {
   public const int BoardSize = 8;
   public const int SquareSize = 34;
-  private readonly Color darkSquareColor = new Color(0.58f, 0.48f, 0.37f);
-  private readonly Color lightSquareColor = new Color(.95f, 0.84f, 0.67f);
+  private readonly Color darkSquareColor = new Color (0.58f, 0.48f, 0.37f);
+  private readonly Color lightSquareColor = new Color (.95f, 0.84f, 0.67f);
 
-  public override void _Ready()
-  {
-    CreateSquares();
+  public override void _Ready () {
+    CreateSquares ();
   }
 
-  protected virtual void CreateSquares()
-  {
-    for (int i = 0; i < BoardSize; i++)
-    {
-      for (int j = 0; j < BoardSize; j++)
-      {
-        var square = new ColorRect();
-        square.Size = new Vector2(SquareSize, SquareSize);
-        square.Color = ((i + j) % 2 == 0) ? lightSquareColor : darkSquareColor;
-        square.GlobalPosition = new Vector2(i * SquareSize, j * SquareSize);
-        AddChild(square);
+  protected virtual void CreateSquares () {
+    for (int i = 0; i < BoardSize; i++) {
+      for (int j = 0; j < BoardSize; j++) {
+        var square = new ColorRect {
+          Size = new Vector2 (SquareSize, SquareSize),
+          Color = ((i + j) % 2 == 0) ? lightSquareColor : darkSquareColor,
+          GlobalPosition = new Vector2 (i * SquareSize, j * SquareSize)
+        };
+        AddChild (square);
       }
     }
   }
--- Scripts/Engine/Moves/HorizontalMove.cs
+++ Scripts/Engine/Moves/HorizontalMove.cs
@@ -1,35 +1,30 @@
-using Godot;
 using System;
+using Godot;
 
-public class HorizontalMove : IMove
-{
-    public HorizontalMove()
-    {
-    }
+public class HorizontalMove : IMove {
+    public HorizontalMove () { }
 
-    public bool IsValidMove(Vector2 currentPosition, Vector2 newPosition, ChessEngine engine)
-    {
+    public bool IsValidMove (Vector2 currentPosition, Vector2 newPosition, ChessEngine engine) {
         if (currentPosition == newPosition) return false; // The positions are the same
 
-        int currentX = (int)currentPosition.X;
-        int currentY = (int)currentPosition.Y;
-        int newX = (int)newPosition.X;
-        int newY = (int)newPosition.Y;
+        int currentX = (int) currentPosition.X;
+        int currentY = (int) currentPosition.Y;
+        int newX = (int) newPosition.X;
+        int newY = (int) newPosition.Y;
 
         if (currentY != newY) return false; // Not moving horizontally
-        if (IsPieceBlockingPath(currentX, currentY, newX, engine)) return false; // There is a piece blocking the path
+        if (IsPieceBlockingPath (currentX, currentY, newX, engine)) return false; // There is a piece blocking the path
 
         return true;
     }
 
-    private static bool IsPieceBlockingPath(int currentX, int currentY, int newX, ChessEngine engine)
-    {
-        int start = Math.Min(currentX, newX) + 1;
-        int end = Math.Max(currentX, newX);
+    private static bool IsPieceBlockingPath (int currentX, int currentY, int newX, ChessEngine engine) {
+        int start = Math.Min (currentX, newX) + 1;
+        int end = Math.Max (currentX, newX);
+        GD.Print ("start: " + start + ", end: " + end);
 
-        for (int i = start; i < end; i++)
-        {
-            if (engine.GetPiece(i, currentY) != null) return true;
+        for (int i = start; i < end; i++) {
+            if (engine.GetPiece (i, currentY) != null) return true;
         }
 
         return false;
